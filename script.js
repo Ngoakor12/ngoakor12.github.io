06ee2ea470186caf5ce2c52eb46c89ee42a9e5a9
ngoakor12 <ngoakor12@gmail.com>
2021-01-22 21:27:32 +0200

    Added showProgressBar function

---
+++ script.js
@@ -0,0 +1,11 @@
+//Progress bar
+// function showProgressBar() {
+//     const progressBar = document.querySelector("#progressBar");
+//     let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+//     window.onscroll = () => {
+//         let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
+//         progressBar.style.height = `${newProgressHeight}%`;
+//         progressBar.style.opacity = `${newProgressHeight}%`;
+//     };
+// }
+// showProgressBar();
