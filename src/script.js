6288dbc09a5ee5393dffc17336cac2936bebf370
ngoakor12 <ngoakor12@gmail.com>
2021-07-04 20:59:16 +0200

    clean script

--- src/script.js
+++ src/script.js
@@ -1,31 +1,23 @@
-//Progress bar
-// function showProgressBar() {
-//     const progressBar = document.querySelector("#progressBar");
-//     let totalPageHeight = document.body.scrollHeight - window.innerHeight;
-//     window.onscroll = () => {
-//         let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
-//         progressBar.style.height = `${newProgressHeight}%`;
-//         progressBar.style.opacity = `${newProgressHeight}%`;
-//     };
-// }
-// showProgressBar();
-
-//Scroll to top function
-var mybutton = document.getElementById("myBtn");
-
+// ---------Scroll to top function---------------//
+let mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+// ----------------------------------------------
